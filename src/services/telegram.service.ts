f0969976191dac112b71bf1fae4c67923431cfe6
Shariar Nehal <113442689+sheikhshariarnehal@users.noreply.github.com>
2025-02-10 19:09:27 +0600

    Update telegram.service.ts

--- src/services/telegram.service.ts
+++ src/services/telegram.service.ts
@@ -53,7 +53,7 @@ export async function sendTelegramMessage(text: string, photo?: string) {
     } else {
       const requestBody = {
         chat_id: TELEGRAM_CHAT_ID,
-        message_thread_id: 204,  // Correct topic ID from the URL
+      //  message_thread_id: 204,  // Correct topic ID from the URL
         text,
         parse_mode: 'HTML',
         disable_web_page_preview: false,
